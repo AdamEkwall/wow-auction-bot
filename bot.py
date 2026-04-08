4959e7ae944bc62fdaa535795b948b435f38679a
AdamEkwall <91548290+AdamEkwall@users.noreply.github.com>
2026-04-08 13:29:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import requests
 url = "https://api.wowauctions.net/items/stats/30d/chromiecraft/mergedAh/45862"
 
 response = requests.get(url)
-data = response.json()
 
-print(data)
+print("STATUS:", response.status_code)
+print("TEXT PREVIEW:")
+print(response.text[:500])  # print first 500 chars
